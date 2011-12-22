35204a6401c9cb10c7a5a5fe1451d968d9647173
Jay Gilmore <me@jaygilmore.ca>
2011-12-22 15:38:18 -0400

    Fixed to correctly reference the $eventPage vs the $eventsPage

--- core/components/eventsx/elements/snippets/evxevent.snippet.php
+++ core/components/eventsx/elements/snippets/evxevent.snippet.php
@@ -1,8 +1,8 @@
 <?php
 $modx->getService('eventsx','EventsX',$modx->getOption('eventsx.core_path',null,$modx->getOption('core_path').'components/eventsx/').'model/eventsx/',$scriptProperties);
 
-//get id of events (overview) page
-$eventsPage = $modx->getOption('evxEventsPage', $scriptProperties, 1);
+//get id of event (single) page
+$eventPage = $modx->getOption('evxEventsPage', $scriptProperties, 1);
 
 //get regex escaped name/url of events (overview) page without /
 //events are below overiew page. e.a. /events/event-name/10
@@ -20,4 +20,4 @@ if(preg_match('/'.$eventsPageRegex.'\/.*\/[0-9]+$/', $_SERVER['REQUEST_URI']))
     }
 }
 
-$modx->sendRedirect($modx->makeUrl($eventsPage));
+$modx->sendRedirect($modx->makeUrl($eventPage));
